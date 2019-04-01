af7b1c658bc404a3b4d37a81d87be03b30da54b6
Saint-Jean Germila <germila@noukod.com>
2019-04-01 19:17:11 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,13 +24,8 @@ class SongsController < ApplicationController
   end
 
   def upload
-  LeadsWorker.perform_async(params[:songs].path)
-  redirect_to songs_path
-
-    # CSV.foreach(params["file"].path, headers: true) do |song|
-    #   Song.create(title: song[0], artist_name: song[1])
-    # end
-    # redirect_to songs_path
+   SongsWorker.perform_async(params[:file].path)
+   redirect_to songs_path
   end
 
   def edit
